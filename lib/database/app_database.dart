260f306048b028c236f8173fa13df6e2948e929b
Taciane Santos <taciane.ssanttos@gmail.com>
2021-10-11 16:32:04 -0300

    Incluindo diretório DAO e classe ContactDao para armazenar comportamentos relacionado ao contato.

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -18,37 +18,3 @@ Future<Database> getDatabase() async {
        //onDowngrade: onDatabaseDowngradeDelete,
       );
 }
-
-Future<int> save(Contact contact) async {
-    final Database db = await getDatabase();
-
-    final Map<String, dynamic> contactMap = Map();
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap);
-
-
-
-  // return getDatabase().then((db){
-  //   final Map<String, dynamic> contactMap = Map();
-  //   contactMap['name'] = contact.name;
-  //   contactMap['account_number'] = contact.accountNumber;
-  //   return db.insert('contacts', contactMap);
-  // });
-}
-
-Future<List<Contact>> findAll() async {
-  final Database db = await getDatabase();
-  final List<Map<String, dynamic>> result = await db.query('contacts');
-
-  final List<Contact> contacts = [];
-  for (Map<String, dynamic> row in result) {
-    final Contact contact = Contact(
-      row['id'],
-      row['name'],
-      row['accountNumber'] ?? 0,
-    );
-    contacts.add(contact);
-  }
-  return contacts;
-}
